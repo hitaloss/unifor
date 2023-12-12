35a3acea84c3ff4638ac93e71af5f4fc10039c4e
hitalo <hitalo.2014.hs@gmail.com>
2023-12-12 04:30:14 -0300

    feat: error handling plus custom types for typescript

--- src/errors/appError.ts
+++ src/errors/appError.ts
@@ -1,7 +1,7 @@
 export class AppError extends Error {
   statusCode;
 
-  constructor(statusCode, message) {
+  constructor(statusCode: number, message: string) {
     super();
     this.statusCode = statusCode;
     this.message = message;
